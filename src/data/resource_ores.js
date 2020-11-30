53646618a89da65e3a9ed68ddf72333d2316779b
caviyacht <caviyacht@users.noreply.github.com>
2020-11-29 23:00:29 -0600

    Added potential stats for platinum ore

--- src/data/resource_ores.js
+++ src/data/resource_ores.js
@@ -2,6 +2,6 @@ export default {
   "15": { type: "resource_ore", name: "Copper Ore" },
   "16": { type: "resource_ore", name: "Silver Ore" },
   "17": { type: "resource_ore", name: "Gold Ore" },
-  "18": { type: "resource_ore", name: "Platinum Ore" },
+  "18": { type: "resource_ore", name: "Platinum Ore", stats: { "hp_boost": 0.08 } },
   "19": { type: "resource_ore", name: "Uranium Ore"}
 };
